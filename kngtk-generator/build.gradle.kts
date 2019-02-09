6be1c1a4fe1d2bdd809f67c42a857b7c24b51d41
Victor Kropp <victor.kropp@gmail.com>
2019-02-09 14:53:38 +0100

    create generator project

---
+++ kngtk-generator/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.3.21"
+}
+
+group = "kngtk-generator"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
